60290199b07e2c3ac5cb671428a56a96d67afedc
Kazuki Mizusawa <ikasumiflash@gmail.com>
2020-08-01 08:07:04 +0900

    added parser and code generator.

--- 9cc.c
+++ 9cc.c
@@ -25,6 +25,36 @@ struct Token{
 // current token
 Token *token;
 
+typedef enum{
+	ND_ADD,
+	ND_SUB,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
 char *user_input;
 
 void error_at(char *loc, char *fmt, ...){
@@ -90,7 +120,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
 	return tok;
 }
 
-// tokenize string p and return it
+// tokenizer
 Token *tokenize(char *p){
 	Token head;
 	head.next = NULL;
@@ -120,37 +150,66 @@ Token *tokenize(char *p){
 	new_token(TK_EOF, cur, p);
 	return head.next;
 }
+// parser
+Node *expr(){
+	Node *node = new_node_num(expect_number());
+
+	for(;;){
+		if(consume('+'))
+			node = new_node(ND_ADD, node, new_node_num(expect_number()));
+		else if(consume('-'))
+			node = new_node(ND_SUB, node, new_node_num(expect_number()));
+		else
+			return node;
+	}
+}
+
+// generator
+void gen(Node *node){
+	if(node->kind == ND_NUM){
+		printf(" push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf(" pop rdi\n");
+	printf(" pop rax\n");
+
+	switch(node->kind){
+	case ND_ADD:
+		printf(" add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf(" sub rax, rdi\n");
+	}
+	printf(" push rax\n");
+}
+
+
+
+
 
 int main(int argc, char **argv){
-	user_input = argv[1];
 	if(argc != 2){
 		error("invalid num of args");
 		return 1;
 	}
 
+	user_input = argv[1];
 	// tokenize
-	token = tokenize(argv[1]);
+	token = tokenize(user_input);
+	Node *node = expr();
 
 	// output header of assembly
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
-	// check that first token is number
-	// output mov operation with the number
-	printf(" mov rax, %d\n", expect_number());
+	gen(node);
 
-	//
-	while(!at_eof()){
-		if(consume('+')){
-			printf(" add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf(" sub rax, %d\n", expect_number());
-	}
-
+	printf(" pop rax\n");
 	printf(" ret\n");
 	return 0;
 }
